4ec6446e130760e0b7bfa65c235de206a221dc80
ZhaiDongbobug <43473343+ZhaiDongbobug@users.noreply.github.com>
2019-12-17 20:39:56 +0800

    Add files via upload

---
+++ PC_Base.ino
@@ -0,0 +1,68 @@
+#define light 13
+int nums[26][6]=
+{
+  {0,1,-1},
+  {1,0,0,0,-1},
+  {1,0,1,0,-1},
+  {1,0,0,-1},
+  {0,-1},
+  {0,0,1,0,-1},
+  {1,1,0,1,-1},
+  {0,0,0,0,-1},
+  {0,0,-1},
+  {0,1,1,1,-1},
+  {1,0,1,-1},
+  {0,1,0,0,-1},
+  {1,1,-1},
+  {1,0,-1},
+  {1,1,1,-1},
+  {0,1,1,0,-1},
+  {1,1,0,1,-1},
+  {0,1,0,-1},
+  {0,0,0,-1},
+  {1,-1},
+  {0,0,1,-1},
+  {0,0,0,1,-1},
+  {0,1,1,-1},
+  {1,0,0,1,-1},
+  {1,0,1,1,-1},
+  {1,1,0,0,-1},
+};
+void Long()
+{
+  digitalWrite(light,HIGH);
+  delay(500);
+  digitalWrite(light,LOW);
+}
+void Short()
+{
+  digitalWrite(light,HIGH);
+  delay(200);
+  digitalWrite(light,LOW);
+}
+string a="NICE";
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(light,OUTPUT);
+  digitalWrite(light,LOW);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0;i<a.length();i++){
+    int index;
+    index = a[i]-'A';
+    int j=0;
+    if(index>=0&&index<26){
+      while(j!=-1){
+        if(nums[index][j] == -1){
+          Long();
+        }
+        else{
+          Short();
+        }
+        j++;
+        delay(500);
+      }
+    }    
+  }
+}
